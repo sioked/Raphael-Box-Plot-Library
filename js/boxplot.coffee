10f0e60efa54f9ecd578eed648354efa2558b9a3
Ed Siok <sioked@gmail.com>
2012-02-25 13:05:35 -0600

    Adding hover functionality to display detailed information

--- js/boxplot.coffee
+++ js/boxplot.coffee
@@ -36,7 +36,7 @@ boxplot.init = (location, data, options, valueFormatter) ->
     "stroke-width": "1"
   ]
   textAttrs = options.textAttrs || [
-    "stroke" : "#000"
+    "fill" : "#000"
     "font-size" : 10
   ]
   canvas = location
@@ -67,20 +67,21 @@ boxplot.calculateYValue = (ypx) ->
   return formatter(value) if formatter
   return value
   
+boxplot.formatValue = (value) ->
+  return formatter(value) if formatter
+  return value
+  
 boxplot.drawLineForValue = (value, title) ->
+  r.setStart()
   ypx = height - margin - yscale * value
-  line = r.path "M#{margin},#{ypx}L#{width},#{ypx}"
-  line.attr defaultAttrs
-  text = r.text (width-margin)/2 + margin, ypx, title
+  text = r.text 1.7*margin, ypx, title
   text.attr textAttrs
   box_dims = text.getBBox()
-  box = r.rect box_dims.x-1, box_dims.y, box_dims.width+2, box_dims.height
-  box.toBack()
+  line = r.path "M#{margin},#{ypx}L#{box_dims.x-1},#{ypx}M#{box_dims.x + box_dims.width+1},#{ypx}L#{width},#{ypx}"
+  line.attr defaultAttrs
+  line.attr {"stroke-width" : 2}
   line.toBack()
-  text.toFront()
-  text.attr "fill", "#000"
-  box.attr { "fill": "#fff", "opacity": .85}
-  r.reference = line
+  r.reference = r.setFinish()
 
 boxplot.drawAxes = (r, data, options) ->
   [x0,y0] = genPoint(0,0)
@@ -102,7 +103,9 @@ boxplot.horizontalHover = ->
   # Set up the hover effect line
   cursor = undefined
   title = undefined
+  interval = 0
   r.canvas.onmousemove = (e)->
+    interval = 0
     y = e.offsetY
     cursor?.remove?()
     title?.remove?()
@@ -110,12 +113,20 @@ boxplot.horizontalHover = ->
     title = undefined
     if 0 < y < height - margin
       r.elems.push cursor = r.path "M#{0},#{y+3}L#{width},#{y+3}"
-      cursor.attr {opacity:.5}
       cursor.attr defaultAttrs
+      cursor.attr {opacity:.5}
       r.elems.push title = r.text margin*1.5, y+20, boxplot.calculateYValue(y+3)
       title.attr textAttrs
-    title  
-      
+    title
+  setInterval( ->
+    if interval > 2
+      cursor?.stop().animate {opacity: 0}, 500, 'linear'
+      title?.stop().animate {opacity: 0}, 500, 'linear'
+    else
+      interval += 1
+  , 1000)
+  r  
+
 boxplot.drawData = (data) ->
   # Add a line for min & max
   # First calculate where on the x-axis to put it
@@ -143,19 +154,36 @@ boxplot.drawData = (data) ->
       r.elems.push vertLine = r.path "M#{min[0]},#{min[1]}L#{max[0]},#{max[1]}"
       vertLine.attr defaultAttrs
       r.elems.push box = r.rect uquart[0]-boxwidth/2, uquart[1], boxwidth, rheight
-      hoverBox = r.rect max[0]-boxwidth*1.5/2, max[1], boxwidth*1.5, min[1]-max[1]
+      hoverBox = r.rect max[0]-boxwidth*5/2, max[1], boxwidth*4, min[1]-max[1]
       hoverBox.attr {"stroke":"none", "fill":"#F00", "fill-opacity":0}
-      box.attr { fill: Raphael.getColor(), 'fill-opacity': 1, title: datapoint.title, stroke : "#333", "stroke-width": 0 }
+      box.attr defaultAttrs
+      box.attr { fill: Raphael.getColor(), 'fill-opacity': 1, title: datapoint.title, "stroke-width": 1 }
       #Display the median line on Top of the box
       r.elems.push medLine = r.path "M#{med[0] - boxwidth/2},#{med[1]}L#{med[0] + boxwidth/2},#{med[1]}"
       medLine.attr defaultAttrs
       
+      r.setStart()
+      offset = 18
+      r.text uquart[0]+offset, uquart[1], "Top 75%\n#{boxplot.formatValue(datapoint.upquart)}"
+      r.text lquart[0]+offset, lquart[1], "Top 25%\n#{boxplot.formatValue(datapoint.loquart)}"
+      if max[1]-20 < 0 then voffset = 10 else voffset = -10
+      console.log max[1], height  
+      r.text max[0]+offset, max[1]+voffset, "Slowest:\n#{boxplot.formatValue(datapoint.max)}"
+      r.text min[0]+offset, min[1]+10, "Fastest:\n#{boxplot.formatValue(datapoint.min)}"
+      titles = r.setFinish()
+      titles.attr {fill: "#FFF", "font-size":10, "text-anchor": "start", opacity: 0}
+      
+      referenceLine = r.reference?.getPointAtLength(0)
+      
       hoverBox.mouseover ->
-        console.log "Hovered!"
-        box.stop().animate { 'stroke': "#FFF", "stroke-width":3}, 400, 'linear'
-        vertLine.stop().animate {'stroke-width' : 3, 'stroke': "#FFF"}, 400, 'linear'
+        r.reference?.animate {'opacity': 0}, 400, 'linear'
+        titles.stop().animate { 'opacity': 1 }, 400, 'linear'
+        box.stop().animate { 'stroke': "#FFF", "stroke-width":2}, 400, 'linear'
+        vertLine.stop().animate {'stroke-width' : 2}, 400, 'linear'
       hoverBox.mouseout ->
-        box.stop().animate { 'stroke-width': 0 }, 200, 'linear'
+        r.reference?.animate {'opacity': 1}, 400, 'linear'
+        titles.stop().animate { 'opacity': 0 }, 400, 'linear'
+        box.stop().animate { 'stroke-width': 1 }, 400, 'linear'
         vertLine.stop().animate {'stroke-width' : 1}, 400, 'linear'
       
       currentMargin=currentMargin + xmargin
--- js/boxplot.js
+++ js/boxplot.js
@@ -40,7 +40,7 @@
     ];
     textAttrs = options.textAttrs || [
       {
-        "stroke": "#000",
+        "fill": "#000",
         "font-size": 10
       }
     ];
@@ -74,24 +74,26 @@
     }
     return value;
   };
+  boxplot.formatValue = function(value) {
+    if (formatter) {
+      return formatter(value);
+    }
+    return value;
+  };
   boxplot.drawLineForValue = function(value, title) {
-    var box, box_dims, line, text, ypx;
+    var box_dims, line, text, ypx;
+    r.setStart();
     ypx = height - margin - yscale * value;
-    line = r.path("M" + margin + "," + ypx + "L" + width + "," + ypx);
-    line.attr(defaultAttrs);
-    text = r.text((width - margin) / 2 + margin, ypx, title);
+    text = r.text(1.7 * margin, ypx, title);
     text.attr(textAttrs);
     box_dims = text.getBBox();
-    box = r.rect(box_dims.x - 1, box_dims.y, box_dims.width + 2, box_dims.height);
-    box.toBack();
-    line.toBack();
-    text.toFront();
-    text.attr("fill", "#000");
-    box.attr({
-      "fill": "#fff",
-      "opacity": .85
+    line = r.path("M" + margin + "," + ypx + "L" + (box_dims.x - 1) + "," + ypx + "M" + (box_dims.x + box_dims.width + 1) + "," + ypx + "L" + width + "," + ypx);
+    line.attr(defaultAttrs);
+    line.attr({
+      "stroke-width": 2
     });
-    return r.reference = line;
+    line.toBack();
+    return r.reference = r.setFinish();
   };
   boxplot.drawAxes = function(r, data, options) {
     var axes, scale, tick, verticals, vheight, vtickpx, x0, y0, _i, _len, _ref, _results;
@@ -127,11 +129,13 @@
     return _results;
   };
   boxplot.horizontalHover = function() {
-    var cursor, title;
+    var cursor, interval, title;
     cursor = void 0;
     title = void 0;
-    return r.canvas.onmousemove = function(e) {
+    interval = 0;
+    r.canvas.onmousemove = function(e) {
       var y;
+      interval = 0;
       y = e.offsetY;
       if (cursor != null) {
         if (typeof cursor.remove === "function") {
@@ -147,15 +151,30 @@
       title = void 0;
       if ((0 < y && y < height - margin)) {
         r.elems.push(cursor = r.path("M" + 0 + "," + (y + 3) + "L" + width + "," + (y + 3)));
+        cursor.attr(defaultAttrs);
         cursor.attr({
           opacity: .5
         });
-        cursor.attr(defaultAttrs);
         r.elems.push(title = r.text(margin * 1.5, y + 20, boxplot.calculateYValue(y + 3)));
         title.attr(textAttrs);
       }
       return title;
     };
+    setInterval(function() {
+      if (interval > 2) {
+        if (cursor != null) {
+          cursor.stop().animate({
+            opacity: 0
+          }, 500, 'linear');
+        }
+        return title != null ? title.stop().animate({
+          opacity: 0
+        }, 500, 'linear') : void 0;
+      } else {
+        return interval += 1;
+      }
+    }, 1000);
+    return r;
   };
   boxplot.drawData = function(data) {
     var currentMargin, datapoint, xmargin, _fn, _i, _len;
@@ -165,7 +184,7 @@
     currentMargin = xmargin;
     r.ylabels = r.set();
     _fn = function(datapoint) {
-      var axis, box, hoverBox, lquart, max, med, medLine, min, rheight, uquart, vertLine, yLabel, ytick;
+      var axis, box, hoverBox, lquart, max, med, medLine, min, offset, referenceLine, rheight, titles, uquart, vertLine, voffset, yLabel, ytick, _ref;
       axis = genPoint(currentMargin, 0);
       r.elems.push(ytick = r.path("M" + axis[0] + "," + (axis[1] - 4) + "L" + axis[0] + "," + (axis[1] + 4)));
       r.ylabels.push(yLabel = r.text(axis[0], axis[1] + margin / 2, datapoint.title));
@@ -180,36 +199,72 @@
       r.elems.push(vertLine = r.path("M" + min[0] + "," + min[1] + "L" + max[0] + "," + max[1]));
       vertLine.attr(defaultAttrs);
       r.elems.push(box = r.rect(uquart[0] - boxwidth / 2, uquart[1], boxwidth, rheight));
-      hoverBox = r.rect(max[0] - boxwidth * 1.5 / 2, max[1], boxwidth * 1.5, min[1] - max[1]);
+      hoverBox = r.rect(max[0] - boxwidth * 5 / 2, max[1], boxwidth * 4, min[1] - max[1]);
       hoverBox.attr({
         "stroke": "none",
         "fill": "#F00",
         "fill-opacity": 0
       });
+      box.attr(defaultAttrs);
       box.attr({
         fill: Raphael.getColor(),
         'fill-opacity': 1,
         title: datapoint.title,
-        stroke: "#333",
-        "stroke-width": 0
+        "stroke-width": 1
       });
       r.elems.push(medLine = r.path("M" + (med[0] - boxwidth / 2) + "," + med[1] + "L" + (med[0] + boxwidth / 2) + "," + med[1]));
       medLine.attr(defaultAttrs);
+      r.setStart();
+      offset = 18;
+      r.text(uquart[0] + offset, uquart[1], "Top 75%\n" + (boxplot.formatValue(datapoint.upquart)));
+      r.text(lquart[0] + offset, lquart[1], "Top 25%\n" + (boxplot.formatValue(datapoint.loquart)));
+      if (max[1] - 20 < 0) {
+        voffset = 10;
+      } else {
+        voffset = -10;
+      }
+      console.log(max[1], height);
+      r.text(max[0] + offset, max[1] + voffset, "Slowest:\n" + (boxplot.formatValue(datapoint.max)));
+      r.text(min[0] + offset, min[1] + 10, "Fastest:\n" + (boxplot.formatValue(datapoint.min)));
+      titles = r.setFinish();
+      titles.attr({
+        fill: "#FFF",
+        "font-size": 10,
+        "text-anchor": "start",
+        opacity: 0
+      });
+      referenceLine = (_ref = r.reference) != null ? _ref.getPointAtLength(0) : void 0;
       hoverBox.mouseover(function() {
-        console.log("Hovered!");
+        var _ref2;
+        if ((_ref2 = r.reference) != null) {
+          _ref2.animate({
+            'opacity': 0
+          }, 400, 'linear');
+        }
+        titles.stop().animate({
+          'opacity': 1
+        }, 400, 'linear');
         box.stop().animate({
           'stroke': "#FFF",
-          "stroke-width": 3
+          "stroke-width": 2
         }, 400, 'linear');
         return vertLine.stop().animate({
-          'stroke-width': 3,
-          'stroke': "#FFF"
+          'stroke-width': 2
         }, 400, 'linear');
       });
       hoverBox.mouseout(function() {
+        var _ref2;
+        if ((_ref2 = r.reference) != null) {
+          _ref2.animate({
+            'opacity': 1
+          }, 400, 'linear');
+        }
+        titles.stop().animate({
+          'opacity': 0
+        }, 400, 'linear');
         box.stop().animate({
-          'stroke-width': 0
-        }, 200, 'linear');
+          'stroke-width': 1
+        }, 400, 'linear');
         return vertLine.stop().animate({
           'stroke-width': 1
         }, 400, 'linear');
